b082d928d165d0767a96ff6f669b1acea125a20e
Baptiste Esteban <baptiste.esteban@epita.fr>
2026-09-22 10:31:54 +0200

    WIP refacto

---
+++ src/ui/callbacks/index/_compute_label_map.py
@@ -0,0 +1,24 @@
+import numpy as np
+from skimage.segmentation import slic
+from skimage.morphology import dilation, footprint_rectangle
+
+from loguru import logger
+
+def compute_label_map(image_data: list, n_segments: int, compactness: float) -> tuple[list, list]:
+    img = np.asarray(image_data)
+
+    N = img.shape[0] * img.shape[1]
+    if n_segments is None or n_segments < 1 or n_segments > N:
+        logger.error(f"Invalid number of segment (Got {n_segments}, expected in [1 - {N}])")
+        return None, None
+
+    if compactness is None or compactness <= 0:
+        logger.error("Compactness must be strictly positive")
+        return None, None
+
+    segments = slic(img, n_segments=n_segments, compactness=compactness)
+    logger.info(f"SLIC computed with {n_segments} segments (Got {segments.max()} segments)")
+    dil = dilation(segments, footprint_rectangle((3, 3)))
+    borders = segments != dil
+
+    return segments.tolist(), borders.tolist()
---
+++ src/ui/pages/_index.py
@@ -0,0 +1,111 @@
+from dash import Dash, html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
+
+from io import BytesIO
+
+from ._page import AbstractPage
+from src.compute import compute_centroid_and_mean, RAG
+
+
+class IndexPage(AbstractPage):
+    def __init__(self, app: Dash):
+        layout = [
+            dbc.Container([
+                html.H1("Hierarchical Segmentation Selector"),
+                html.Hr(),
+                dbc.Card(
+                    dbc.Row([
+                        dbc.Col(dcc.Upload(children=[dbc.Button("Open Image")], id="upload-image", accept="image/*")),
+                        dbc.Col(dbc.Button("Download Segmentation", id="download-segmentation-button")),
+                        dbc.Col([dbc.Input(type="color", id="selection-color", value="#FF0000")])
+                    ]),
+                    body=True),
+                html.Hr(),
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Card([
+                            dbc.Label("Number of segments"),
+                            dbc.Input(id="n-segments-input", type="number", value=10)
+                        ],
+                        body=True)
+                    ]),
+                    dbc.Col([
+                        dbc.Card([
+                            dbc.Label("Compactness"),
+                            dbc.Input(id="compactness-input", type="number", value=10)
+                        ],
+                        body=True)
+                    ])
+                ]),
+                dbc.Row([
+                    dbc.Col(dcc.Graph(id="image-graph", style={'width': '100%', 'height': 'calc(100vh - 400px)'})),
+                    dbc.Col(html.H3("TODO ! (Dendrogram)"))
+                ]),
+                
+            ], style={'display': 'flex', 'flexDirection': 'column', 'height': '100vh'}),
+            dcc.Store(id="image-data"),
+            dcc.Store(id="label-map-data"),
+            dcc.Store(id="selected-regions-data"),
+            dcc.Store(id="border-data"),
+            dcc.Download(id="segmentation-download")
+        ]
+        super().__init__(app, "Index", layout)
+
+    def _register_callbacks(self):
+        from src.ui.callbacks.index import (
+            load_image,
+            compute_label_map,
+            display_image,
+            select_region,
+            download_segmentation
+        )
+
+        @self._app.callback(
+            Output("image-data", "data"),
+            Input("upload-image", "contents"),
+            prevent_initial_call=True
+        )
+        def load_image_callback(contents):
+            return load_image(contents)
+
+        @self._app.callback(
+            Output("label-map-data", "data"),
+            Output("border-data", "data"),
+            Input("image-data", "data"),
+            Input("n-segments-input", "value"),
+            Input("compactness-input", "value"),
+            prevent_initial_call=True
+        )
+        def compute_label_map_callback(image_data, n_segments, compactness):
+            return compute_label_map(image_data, n_segments, compactness)
+
+        @self._app.callback(
+            Output("image-graph", "figure"),
+            Input("image-data", "data"),
+            Input("label-map-data", "data"),
+            Input("selected-regions-data", "data"),
+            Input("border-data", "data"),
+            Input("selection-color", "value")
+        )
+        def display_image_callback(image_data, label_map, selected_regions, border, selected_color):
+            return display_image(image_data, label_map, selected_regions, border, selected_color)
+
+        @self._app.callback(
+            Output("selected-regions-data", "data"),
+            Input("image-graph", "clickData"),
+            Input("border-data", "data"),
+            State("selected-regions-data", "data"),
+            State("label-map-data", "data"),
+            prevent_initial_call=True
+        )
+        def select_region_callback(click, borders, selected_regions, label_map):
+            return select_region(click, borders, selected_regions, label_map)
+
+        @self._app.callback(
+            Output("segmentation-download", "data"),
+            Input("download-segmentation-button", "n_clicks"),
+            State("selected-regions-data", "data"),
+            prevent_initial_call=True
+        )
+        def download_segmentation_callback(click, selected_regions_data):
+            return download_segmentation(click, selected_regions_data)
